5576a4d00e6681213cc1219be0096ca0c8e7f9e4
Frank Schmidt <frankschmidt902@gmail.com>
2020-10-17 15:52:49 -0300

    add thunbnail to item view

--- components/auctionItem.js
+++ components/auctionItem.js
@@ -11,6 +11,7 @@ function AuctionItem({ id }) {
 
   async function fetchEntries() {
     const item = await client.getEntry(id);
+    console.log(item.fields);
     setItem(item);
   }
 
@@ -21,11 +22,64 @@ function AuctionItem({ id }) {
     }
   }, [id]);
 
+  const [pictures, setPictures] = useState([]);
+  useEffect(() => {
+    if (!item) return;
+    let pictures = [];
+    item.fields.images.map((image) => {
+      pictures.push(image.fields);
+    });
+    console.log(pictures);
+    setPictures(pictures);
+  }, [item]);
+
+  const [description, setDescription] = useState("");
+  useEffect(() => {
+    if (!item) return;
+    let description = "";
+    item.fields.description.content.map((c) => {
+      c.content.map((x) => {
+        description += x.value;
+      });
+    });
+    setDescription(description);
+  }, [item]);
+
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col p-4">
       {!!item ? (
         <div>
+          <div
+            className="h-64 w-64 mb-6"
+            style={{
+              backgroundImage:
+                "url(" + item.fields.poster.fields.file.url + ")",
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+          ></div>
+
           <h1 className="font-bold text-xl">{item.fields.title}</h1>
+          <p className="text-gray-900">{description}</p>
+          <div>
+            {pictures.map((image, idx) => {
+              <div>
+                <div
+                  className="h-64 w-64 mb-6"
+                  style={{
+                    backgroundImage: "url(" + image.file.url + ")",
+                    backgroundPosition: "center",
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
+                  }}
+                ></div>
+                <h4>
+                  title: {image.title} {image.file.url}
+                </h4>
+              </div>;
+            })}
+          </div>
         </div>
       ) : (
         "loading.."
