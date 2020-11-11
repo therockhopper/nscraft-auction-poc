ba5d548ad1166070ac79770d4bb4a9afd5db2061
fschmidt <frank.schmidt@redspace.com>
2020-11-11 19:23:01 -0400

    update card styles

--- components/auctionItemCard.js
+++ components/auctionItemCard.js
@@ -4,29 +4,41 @@ function AuctionItem({item}) {
     <Link href={`/?itemId=${item.sys.id}`} as={`/item/${item.sys.id}`}>
       <div className="flex flex-col bg-gray-100 rounded overflow-hidden grow cursor-pointer shadow-xl text-gray-900 w-full h-full max-w-sm">
         <div
-          className="h-56 w-full"
           style={{
+            height: '25vh',
+            width: '100%',
             backgroundImage: 'url(' + item.fields.poster.fields.file.url + ')',
             backgroundPosition: 'center',
             backgroundSize: 'cover',
             backgroundRepeat: 'no-repeat',
           }}></div>
-        <div className="p-2 flex flex-col">
+        <div className="p-2 flex flex-col h-48">
           <h2 className="pb-2">{item.fields.title}</h2>
-          <span> {item.fields.subtitle || 'subtitle'}</span>
-          <span> {item.fields.company || 'company name'}</span>
-          <span className="text-gray-90"> #{item.fields.itemNumber || 0} </span>
-          <span> ${item.fields.value || 'N/A'}</span>
-          <span className="text-gray-90">
-            {item.fields.itemNumber || 0 < 40 ? 'Day one item' : 'Day two item'}
+          <span className="font-semibold">
+            {item.fields.subtitle || 'subtitle'}
           </span>
+          <span> {item.fields.company || 'company name'}</span>
+          <span> ${item.fields.value || 'N/A'}</span>
         </div>
-        <div className="flex p-2">
-          {item.fields.taken ? (
-            <span className="text-red-400">Not Available</span>
-          ) : (
-            <span className="text-green-600">Available</span>
-          )}
+        <div className="flex p-2 justify-between">
+          <div>
+            <span className="text-gray-90">
+              {' '}
+              #{item.fields.itemNumber || 0}{' '}
+            </span>
+            {item.fields.taken ? (
+              <span className="text-red-400">Not Available</span>
+            ) : (
+              <span className="text-green-600">Available</span>
+            )}
+            <div>
+              <span className="text-gray-90">
+                {item.fields.itemNumber || 0 < 40
+                  ? 'Day one item'
+                  : 'Day two item'}
+              </span>
+            </div>
+          </div>
         </div>
       </div>
     </Link>
