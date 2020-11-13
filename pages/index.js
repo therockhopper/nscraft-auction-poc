d3654976d1f3d99e7ef5bbcded0ccd795c92ee3b
fschmidt <frank.schmidt@redspace.com>
2020-11-13 09:10:34 -0400

    feat - move content fetch to server side

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,7 @@ const client = require('contentful').createClient({
   accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
 });
 
-function HomePage() {
-  const [homeHeroContent, setHomeHeroContent] = useState('');
-  const [homeHeroBody, setHomeHeroBody] = useState({__html: ''});
+function HomePage({homeHeroContent, homeHeroBody}) {
   const [auctionItems, setAuctionItems] = useState([]);
 
   async function fetchEntries() {
@@ -21,28 +19,12 @@ function HomePage() {
     if (entries.items) return entries.items;
   }
 
-  async function fetchHomeHeroContent() {
-    const resp = await client.getEntry('1RrCILWJJh8DwK3QtINEiy');
-    if (resp && resp.fields) return resp.fields;
-  }
-
-
   useEffect(() => {
-    async function getHomeContent() {
-      const content = await fetchHomeHeroContent()
-      setHomeHeroContent(content)
-
-      const rendredHTML = await documentToHtmlString(content.heroBodyText);
-      setHomeHeroBody({__html: rendredHTML});
-    }
-    getHomeContent()
-
     async function getAuctionItems() {
       const allAuctionItems = await fetchEntries();
       setAuctionItems(allAuctionItems);
     }
     getAuctionItems();
-
 
     const interval = setInterval(() => {
       getAuctionItems();
@@ -50,31 +32,42 @@ function HomePage() {
     return () => clearInterval(interval);
   }, []);
 
-
   Modal.setAppElement('#__next');
 
   const router = useRouter();
 
   return (
     <div className="flex flex-col">
-    <div className="flex text-gray-100 bg-blue-600 pb-2 border-b-2 border-blue-700 px-4 py-2">
-    <h1 className="text-2xl font-semibold">{homeHeroContent.navbarTitle}</h1>
-    </div>
-    <div className="flex bg-blue-600 h-full">
-    <div className="flex flex-col justify-between text-white px-4 py-2 w-1/2">
-    <div dangerouslySetInnerHTML={homeHeroBody}></div>
-    </div>
-    <img className="w-1/2" src="/svgs/home.svg" alt="home image" />
-    </div>
-    <Modal
-    isOpen={!!router.query.itemId}
-    onRequestClose={() => router.push('/')}
-    contentLabel="Item modal">
-    <AuctionItem id={router.query.itemId}></AuctionItem>
-    </Modal>
-    <AuctionItems items={auctionItems} />
+      <div className="flex text-gray-100 bg-blue-600 pb-2 border-b-2 border-blue-700 px-4 py-2">
+        <h1 className="text-2xl font-semibold">
+          {homeHeroContent.navbarTitle}
+        </h1>
+      </div>
+      <div className="flex bg-blue-600 h-full">
+        <div className="flex flex-col justify-between text-white px-4 py-2 w-1/2">
+          <div dangerouslySetInnerHTML={homeHeroBody}></div>
+        </div>
+        <img className="w-1/2" src="/svgs/home.svg" alt="home image" />
+      </div>
+      <Modal
+        isOpen={!!router.query.itemId}
+        onRequestClose={() => router.push('/')}
+        contentLabel="Item modal">
+        <AuctionItem id={router.query.itemId}></AuctionItem>
+      </Modal>
+      <AuctionItems items={auctionItems} />
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const resp = await client.getEntry('1RrCILWJJh8DwK3QtINEiy');
+  const homeHeroContent = resp.fields;
+  const rendredHTML = await documentToHtmlString(resp.fields.heroBodyText);
+  const homeHeroBody = {__html: rendredHTML};
+  return {
+    props: {homeHeroContent, homeHeroBody},
+  };
 }
 
 export default HomePage;
