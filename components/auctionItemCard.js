f331950ed59ae28967767720702adf9d950a5581
Frank Schmidt <frankschmidt902@gmail.com>
2022-01-31 17:34:00 -0400

    adjust card

--- components/auctionItemCard.js
+++ components/auctionItemCard.js
@@ -15,36 +15,37 @@ function AuctionItem({item}) {
         hoverable
         style={{minWidth: '320px', width: '25vw', maxWidth: '500px' }}
         cover={<img alt="example" src={item.fields.poster.fields.file.url} style={{objectFit: 'cover', height: '25vw', maxHeight: '350px' }} />} >
-        <div className="flex flex-col px-4 pt-6">
-          <p className="text-blue-500 font-semibold text-xs mb-1 leading-none">
+        <div className="flex flex-col">
+          <p className="text-blue-500 font-semibold text-xs">
             {item.fields.company}
           </p>
-          <h3 className="font-semibold mb-2 text-xl leading-tight sm:leading-normal">
+          <h3 className="font-semibold text-lg leading-tight sm:leading-normal">
             {item.fields.title}
           </h3>
         </div>
-        <div className="flex justify-between p-4">
-          <div className="flex flex-col ">
+        <div className="flex justify-between">
+          <div className="flex flex-col justify-end">
             {item.fields.taken ? (
               <span className="text-red-400">Acquired</span>
             ) : (
               <span className="text-green-600">Available</span>
             )}
 
-            <p className="leading-none">
+            <p className="leading-none mb-0">
               ${item.fields.value}{' '}
               <span className="text-sm italic text-gray-700">value</span>
             </p>
           </div>
           <div className="flex flex-col items-end">
-            <span className="text-sm font-semibold text-gray-700">
-              {item.fields.subtitle}
-            </span>
             <span className="text-gray-90 text-sm flex justify-end">
               {item.fields.itemNumber > 80 && 'Silent Auction '}
               {item.fields.itemNumber && '#'}
               {item.fields.itemNumber && item.fields.itemNumber}
             </span>
+            <span className="text-sm font-semibold text-gray-700">
+              {item.fields.subtitle}
+            </span>
+
             <span className="text-gray-90 text-sm flex justify-end">
               {item.fields.year ? item.fields.year : '2020'}
             </span>
